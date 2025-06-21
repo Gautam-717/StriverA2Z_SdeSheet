779983036a23e286e82e3ba46fe497f18e9ca9c8
Gautam <hellogautam16@gmail.com>
2025-06-21 16:37:54 +0530

    _z10_MinimumElementInRotatedSortedArray

--- src/BinarySearch/_1easy/_z10_MinimumElementInRotatedSortedArray.java
+++ src/BinarySearch/_1easy/_z10_MinimumElementInRotatedSortedArray.java
@@ -1,6 +1,6 @@
 package BinarySearch._1easy;
 
-public class _10_MinimumElementInRotatedSortedArray {
+public class _z10_MinimumElementInRotatedSortedArray {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
         System.out.println(Minimum(arr));
