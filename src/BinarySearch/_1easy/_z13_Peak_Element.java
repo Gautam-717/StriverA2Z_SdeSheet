dc1c84715e497f7e7dafff5c95784d1be0244589
Gautam <hellogautam16@gmail.com>
2025-06-21 17:53:54 +0530

    PeakElement

--- src/BinarySearch/_1easy/_z13_Peak_Element.java
+++ src/BinarySearch/_1easy/_z13_Peak_Element.java
@@ -1,6 +1,6 @@
 package BinarySearch._1easy;
 
-public class _13_Peak_Element {
+public class _z13_Peak_Element {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,5,1};
         System.out.println(PeakElement(arr));
